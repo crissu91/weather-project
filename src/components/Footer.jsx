7bcf83a436273a246707301c4c85eea5054ae04f
crissu91 <cristinaf.stefan@gmail.com>
2023-09-25 14:10:18 +0100

    add footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Footer() {
+  return (
+      <footer className='w-full py-8'>
+          <div className="text-center text-sm text-white pt-8 sm:pt-12 font-light flex items-center justify-center">
+          Created by Cristina Stefan, 2023
+        </div>
+    </footer>
+  )
+}
+
+export default Footer
